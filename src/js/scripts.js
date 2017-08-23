1f67a7b70efc688183114b9483ae5a0591ec3a11
Joe Karasek <joseph.karasek@gmail.com>
2017-08-23 10:32:18 -0700

    internal QA edits

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,75 +13,7 @@
     effect: 'fade',
     speed: 500
   });
-  HelloWorldDevsTysonSteele.tourCarousel('.js-tour-carousel', {
-    items: 1,
-    margin: 40,
-    navText: [
-      '<img src="assets/tour-left.png">',
-      '<img src="assets/tour-right.png">'
-    ],
-    autoHeight: true
-  });
-  HelloWorldDevsTysonSteele.tourCarousel('.js-services-carousel', {
-    items: 1,
-    margin: 40,
-    nav: true,
-    navText: [
-      '<img src="assets/tour-left.png">',
-      '<img src="assets/tour-right.png">'
-    ],
-    autoHeight: true
-  });
-  HelloWorldDevsTysonSteele.tourCarousel('.js-services-carousel--two', {
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 500,
-    autoplayTimeout: 2000,
-    autoplayHoverPause:false,
-    autoWidth: false,
-    nav: true,
-    navText: [
-      '<img src="assets/tour-left.png">',
-      '<img src="assets/tour-right.png">'
-    ],
-    dots: false,
-    margin: 15,
-    slideBy: 1,
-    responsive: {
-      550: {
-        items: 2
-      },
-      768: {
-        items: 3
-      },
-      1200: {
-        items: 4
-      }
-    }
-  });
-  HelloWorldDevsTysonSteele.tourCarousel('.js-services-carousel--mobile', {
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 500,
-    autoplayTimeout: 2000,
-    autoplayHoverPause:false,
-    autoWidth: false,
-    nav: true,
-    navText: [
-      '<img src="assets/tour-left.png">',
-      '<img src="assets/tour-right.png">'
-    ],
-    dots: false,
-    margin: 15,
-    slideBy: 1,
-    responsive: {
-      550: {
-        items: 2
-      }
-    }
-  });
+
   HelloWorldDevsTysonSteele.associationCarousel('.js-associations-carousel', {
     items: 2,
     loop: true,
@@ -109,35 +41,10 @@
       }
     }
   });
-  HelloWorldDevsTysonSteele.pyramidCarousel('.js-team-carousel', {
-    items: 1,
-    autoplay: true,
-    navText: [
-      '<img src="assets/tour-left.png">',
-      '<img src="assets/tour-right.png">'
-    ],
-    responsive: {
-      550: {
-        items: 2
-      },
-      768: {
-        items: 3
-      },
-      992: {
-        items: 4
-      }
-    },
-    margin: 10
-  });
   HelloWorldDevsTysonSteele.updateCopyright('.js-copyright-year');
 
-  $('#smile-gallery-remote').click(function(event) {
-    event.preventDefault();
-    $('#smile-gallery-modal').click();
-  });
-
   // ======= Google Maps =======
-  HelloWorldDevsTysonSteele.googleMap('#google-map5', '47.7381209,-122.6392155', '47.7381209,-122.6392155');
+  HelloWorldDevsTysonSteele.googleMap('#google-map5', '47.647946,-122.333794', '47.647946,-122.333794');
 
 
 }(jQuery, HelloWorldDevsTysonSteele));
